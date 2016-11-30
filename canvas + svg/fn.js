a69c4f31560868cdfbe5529ca84913868a318206
Kallirroi <kallirroi.retzepi@gmail.com>
2016-11-30 10:21:55 +0100

    moved avoidCollisions from the actions to the helper functions

--- canvas + svg/fn.js
+++ canvas + svg/fn.js
@@ -18,5 +18,24 @@ var fn = {
    */
   identity: function(x) {
     return x;
-  }
+  },
+  /**
+   * fn.avoidCollisions
+   */
+  avoidCollisions: function(_state) {  /* prevent collission*/
+              var box = d3.select("text").node().getBBox();
+              _state.data.filter(function(d) {
+                return d.y > box.y;
+              })
+              .filter(function(d) {
+                return d.x > box.x;
+              })
+              .filter(function(d) {
+                return d.x < box.x + box.width
+              })
+              .map(function(d){
+                d.fy = d.y; 
+                d.fx = d.x;
+              })
+            }
 }
