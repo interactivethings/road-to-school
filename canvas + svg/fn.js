f3bae3e4e1db5b946421073fa2d6b3521495c8f6
Kallirroi <kallirroi.retzepi@gmail.com>
2016-11-30 17:27:35 +0100

    moved aux functions to fn.js

--- canvas + svg/fn.js
+++ canvas + svg/fn.js
@@ -14,9 +14,30 @@ var fn = {
     return undefined;
   },
   /**
+   * fn.fixAtBottom
+   */
+  fixAtBottom: function(d) {
+    d.fx = d.x;
+    d.fy = d.y - Math.abs(Math.random()) * 10;
+    return d;
+  },
+  /**
    * fn.identity
    */
   identity: function(x) {
     return x;
+  },
+  /**
+  * fn.updateCircle
+  */
+  updateCircle: function() {
+    circle
+      .attr('cx', circleDatum.x)
+      .attr('cy', circleDatum.y);
+    text
+      .attr('x', circleDatum.x)
+      .attr('y', circleDatum.y);
+
+    return circle, text;
   }
 }
