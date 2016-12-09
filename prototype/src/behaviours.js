801bcfc480f23ec7d292b113272279e6fc0c2caa
Kallirroi <kallirroi.retzepi@gmail.com>
2016-12-09 09:49:06 +0100

    implemented a d3.interval to simulate a 'bomb' force - it is executed more than once evocation - FIX

--- prototype/src/behaviours.js
+++ prototype/src/behaviours.js
@@ -18,14 +18,13 @@ export function baseline(force, data, {width, height}) {
     .force('collideSchool', isolate(data, d3.forceCollide(), function(d) { return d.type === 'school' ; }).radius(function(d) { return d.r + 2; }).iterations(2).strength(0.8)) 
     .force('collideNoSchool', isolate(data, d3.forceCollide(), function(d) { return d.type === 'noSchool' ; }).radius(function(d) { return d.r +2; }).iterations(2).strength(0.8)) 
     .velocityDecay(0.3 )
-    .alphaTarget(0.3)
+    .alphaTarget(0.3);
 }
 
 export function outOfSchool(force, data, {width, height}) {
   console.log('outOfSchool')
   
   force
-
     .force('xSchool', isolate(data, d3.forceX(width/2), function(d) { return d.type === 'school' ; }).strength(function(d,i) { return d.id/(data.length*8); }))
     .force('ySchool', isolate(data, d3.forceY(height/2), function(d) { return d.type === 'school' ; }).strength(function(d,i) { return d.id/(data.length*8); }))
     .force('xNoSchool', isolate(data, d3.forceX(function(d,i) { return Math.sin(i) * R * getVariation(0.8,1) + (width/2);} ), function(d) { return d.type === 'noSchool'; }).strength(0.2))      
@@ -33,7 +32,21 @@ export function outOfSchool(force, data, {width, height}) {
     .force('collideSchool', isolate(data, d3.forceCollide(), function(d) { return d.type === 'school'; }).radius(3).iterations(2).strength(0.8))
     .force('collideNoSchool', isolate(data, d3.forceCollide(), function(d) {return d.type === 'noSchool'; }).radius(0.1).iterations(2).strength(0.5))
     .alphaTarget(0.8)
-    .velocityDecay(0.3)
+    .velocityDecay(0.3);
+
+    d3.interval(function(){
+        console.log('bomb')
+
+        var bombForce = d3.forceSimulation(data);
+        bombForce.force('bombX', d3.forceX(function(d,i) { return Math.sin(i) *  R * getVariation(1,1.2) + width/2 * getVariation(0.9,1.2);} ).strength(-0.1))
+        .force('bombY', d3.forceY(function(d,i) { return Math.cos(i) * R * getVariation(1,1.2) + height/2 * getVariation(0.9,1.2);} ).strength(-0.1))
+        .alphaTarget(0.2)
+        .velocityDecay(0.8)
+        .stop()
+        .tick();
+
+    }, 5000);
+
 }
 
 export function backToSchool(force, data, {width, height}) {
@@ -47,18 +60,6 @@ export function backToSchool(force, data, {width, height}) {
     .force('collideSchool', isolate(data, d3.forceCollide(), function(d) { return d.type === 'school' ; }).radius(function(d) { return 2* d.r; }).iterations(2).strength(0.8)) 
     .force('collideNoSchool', isolate(data, d3.forceCollide(), function(d) { return d.type === 'noSchool' ; }).radius(function(d) { return 2* d.r; }).iterations(2).strength(0.8))
     .velocityDecay(0.7)
-    .alphaTarget(0.8) 
-
-}
-
-export function bomb(force, data, {width, height}) {
-    // console.log('bomb')
-    d3.forceSimulation(data)
-        .alphaTarget(0.5)
-        .force('bombX', d3.forceX(function(d,i) { return Math.sin(i) * R/10 + width/2;} ).strength(-0.2))
-        .force('bombY', d3.forceY(function(d,i) { return Math.cos(i) * R/10 + height/2;} ).strength(-0.2))
-        .velocityDecay(0.5)
-        .stop()
-        .tick();
+    .alphaTarget(0.8); 
 
 }
