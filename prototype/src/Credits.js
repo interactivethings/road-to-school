c7d051518947ba2f8363b8abfed1a5499d42c3e3
Kallirroi <kallirroi.retzepi@gmail.com>
2016-12-14 12:31:25 +0100

    CSS changes, better icons added and work

--- prototype/src/Credits.js
+++ prototype/src/Credits.js
@@ -4,24 +4,28 @@ class Credits extends Component {
   render() {
     return (
 			<div className="Credits">  
+				<div className="Credits-Methods"> Methods
+		     		<div>Every word in the visualization corresponds to roughly 100,000 Syrian children. </div>
+	     		</div>
 	     		<div className="Credits-Donate"> Donate
 		     		<ul>
-		     			<li>https://donate.unhcr.org/int-en/syria-chf/</li>	
-		     			<li>http://arsis.gr/en/</li>	
-		     			<li>http://childrenofsyria.info/</li>	
+		     			<li> <a href="https://donate.unhcr.org/int-en/syria-chf/">https://donate.unhcr.org/int-en/syria-chf </a></li>	
+		     			<li> <a href="http://arsis.gr/en/">http://arsis.gr/en/</a></li>	
+		     			<li> <a href="http://childrenofsyria.info/"> http://childrenofsyria.info/</a></li>	
 		     		</ul>
 	     		</div>
-	     		<br/>
-	     		<br/>
 				<div className="Credits-Sources">Journaling sources
 		     		<ul>
-		     			<li>http://childrenofsyria.info/</li>	
-		     			<li>http://data.unhcr.org/mediterranean/country.php?id=83/</li>	
-		     			<li>http://data.unhcr.org/syrianrefugees/country.php?id=8 </li>	
-		     			<li>http://www.aljazeera.com/news/2016/10/easy-answers-syrian-students-lebanon-161006075947100.html </li>	
-		     			<li>https://www.hrw.org/news/2016/09/15/refugee-education-crisis-requires-global-employment-strategy</li>	
-		     			<li>https://www.hrw.org/news/2015/11/08/turkey-400000-syrian-children-not-school</li>	
+		     			<li> <a href="http://childrenofsyria.info/"> http://childrenofsyria.info/ </a></li>	
+		     			<li> <a href="http://data.unhcr.org/mediterranean/country.php?id=83/">http://data.unhcr.org/mediterranean/country.php?id=83/</a></li>	
+		     			<li> <a href="http://data.unhcr.org/syrianrefugees/country.php?id=8">http://data.unhcr.org/syrianrefugees/country.php?id=8 </a></li>	
+		     			<li> <a href="http://www.aljazeera.com/news/2016/10/easy-answers-syrian-students-lebanon-161006075947100.html"> http://www.aljazeera.com/news/2016/10/easy-answers-syrian-students-lebanon-161006075947100.html</a></li>	
+		     			<li> <a href="https://www.hrw.org/news/2016/09/15/refugee-education-crisis-requires-global-employment-strategy">https://www.hrw.org/news/2016/09/15/refugee-education-crisis-requires-global-employment-strategy</a></li>	
+		     			<li> <a href="https://www.hrw.org/news/2015/11/08/turkey-400000-syrian-children-not-school">https://www.hrw.org/news/2015/11/08/turkey-400000-syrian-children-not-school</a></li>	
 		     		</ul>
+	     		</div>
+	     		<div className="Credits-Credits"> Credits
+		     		<div>love to every interactive thing </div>
 	     		</div>			
 			</div>
     );
